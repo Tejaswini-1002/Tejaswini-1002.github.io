6292da9a736cd07a8168a2436e3bf218405d88a1
Tejaswini-1002 <tejaswinipadhy2001@gmail.com>
2024-08-04 11:57:26 +0530

    add banner images

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar'
 
+const src1 = "../Images/hero image 2.png";
+const src2 = "../Images/Hero image 1.png";
+
 function Banner(){
     return(
     <div style={{"backgroundColor":"#C9C0B1"}} className="h-screen px-5 py-4 overflow-hidden">
         <Navbar />
         <div className="flex justify-center ">
             <p style={{"color":"#1F3923"}} className="font-medium text-2xl my-auto font-mono">'As interesting<br /> as a plant'</p>
-            <img alt="hero1" src="../Images/hero image 2.png" className="" />
-            <img alt="hero2" src="../Images/Hero image 1.png" className="" />
+            <img alt="hero1" src={src1} className="" />
+            <img alt="hero2" src={src2} className="" />
         </div>
     </div>
     )
