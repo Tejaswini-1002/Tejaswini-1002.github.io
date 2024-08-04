d57837717bec90258f751455345132071815da82
Tejaswini-1002 <tejaswinipadhy2001@gmail.com>
2024-08-04 12:15:18 +0530

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Navbar from './Navbar'
-import src1 from "../hero image 2.png"
-import src2 from "../Hero image 1.png"
-
-// const src1 = "../Images/hero image 2.png";
-// const src2 = "../Images/Hero image 1.png";
 
 function Banner(){
+    const images = [
+        {
+            id:1,
+            src: "../Images/Hero image 1.png"
+        },
+        {
+            id:2,
+            src: "../Images/hero image 2.png"
+        }
+    ];
+
     return(
     <div style={{"backgroundColor":"#C9C0B1"}} className="h-screen px-5 py-4 overflow-hidden">
         <Navbar />
         <div className="flex justify-center ">
             <p style={{"color":"#1F3923"}} className="font-medium text-2xl my-auto font-mono">'As interesting<br /> as a plant'</p>
-            <img alt="hero1" src={src1} className="" />
-            <img alt="hero2" src={src2} className="" />
+            <img alt="hero1" src={images[0].src} className="" />
+            <img alt="hero2" src={images[1].src} className="" />
         </div>
     </div>
     )
